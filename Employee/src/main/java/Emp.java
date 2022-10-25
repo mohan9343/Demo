e5d27944c15fcd37b4c5187d74eadb526fbc0a66
mohan9343 <mohankrishnag24@gmail.com>
2022-10-25 22:46:28 +0530

    emp first

---
+++ Employee/src/main/java/Emp.java
@@ -0,0 +1,10 @@
+
+public class Emp {
+int Emp_Id=1;
+String Emp_name="Mohan";
+public static void main(String[] args) {
+	Emp emp=new Emp(); 
+	System.out.println(emp.Emp_Id);
+	System.out.println(emp.Emp_name);
+}
+}
